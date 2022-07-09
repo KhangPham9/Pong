c01d62b466e7d963e6fdcd67c096dd7f9cfb98f5
KhangPham9 <khangpham9999@csu.fullerton.edu>
2022-07-09 13:30:09 -0700

    added second paddle

--- CLIENT/app/js/game.js
+++ CLIENT/app/js/game.js
@@ -5,7 +5,8 @@ class Game {
   constructor(ctx) {
     this.ctx = ctx;
     // console.log(this.ctx.fillStyle);
-    this.paddle = new Paddle(this.ctx, 50, 50, 100, 25, 'grey');
+    this.paddle1 = new Paddle(this.ctx, 50, 50, 100, 25, 'grey');
+    this.paddle2 = new Paddle(this.ctx, 50, 725, 100, 25, 'lightblue');
     this.ball = new Ball(this.ctx, 15, 'red', 50, 50, 2, 1, 2);
     // console.log(this.ctx.fillStyle);
 
@@ -23,7 +24,8 @@ class Game {
     this.update();
 
     // console.log(this.ctx.fillStyle);
-    this.paddle.draw();
+    this.paddle1.draw();
+    this.paddle2.draw();
     this.ball.draw();
 //
     // console.log(this.ctx.fillStyle);
