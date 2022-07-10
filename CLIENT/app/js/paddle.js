ce1bdb3bca7c9e00e46cdea3d9d8d3bb3dfebf45
KhangPham9 <khangpham9999@csu.fullerton.edu>
2022-07-09 20:16:56 -0700

    created the paddles class that controls both paddles.

--- CLIENT/app/js/paddle.js
+++ CLIENT/app/js/paddle.js
@@ -1,14 +1,40 @@
 import Rectangle from './rectangle.js';
+import Input from './input.js';
+import Vector from './vector.js';
 
-class Paddles {}
 
 class Paddle {
   constructor(ctx, settings) {
     this.ctx = ctx;
     this.rect = new Rectangle(ctx, settings.paddle);
+    this.vel = new Vector(0 ,0);
+    this.speedFactor = settings.speedFactor;
+    this.input = new Input(this.vel, settings.keys);
   }
 
   draw() { this.rect.draw(); }
+
+  update() {
+    this.rect.topLeftPos.x += this.vel.x * this.speedFactor;
+    this.rect.topLeftPos.y += this.vel.y * this.speedFactor;
+  }
 }
 
-export default Paddle;
+class Paddles {
+  constructor(ctx, topPaddleSettings, bottomPaddleSettings) {
+    this.ctx = ctx;
+    this.paddle1 = new Paddle(this.ctx, topPaddleSettings);
+    this.paddle2 = new Paddle(this.ctx, bottomPaddleSettings); 
+  }
+
+  draw() {
+    this.paddle1.draw();
+    this.paddle2.draw();
+  }
+
+  update() {
+    this.paddle1.update();
+    this.paddle2.update();
+  }
+}
+export default Paddles;
